f982c47898b06e19c32692f6c8f2d354b2d61f9d
lin979 <86038968+linwy979@users.noreply.github.com>
2023-12-20 22:14:45 +0800

    Add files via upload

---
+++ hw5/main 3.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   int x=1;
+   for(int y=1;(y<=9&&x<=9);y++){
+       printf("%d*%d=%d\t",x,y,x*y);
+       if(y==9){
+           x++;
+           y=1;
+           printf("\n");
+       }
+   }
+        
+    
+    return 0;
+}
